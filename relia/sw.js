064e45f1687f9bc0a258843c7c9e7382ba1cfa8f
oguzcelik93 <oguzcelik93@gmail.com>
2026-08-19 12:59:38 +0200

    relia: güncelleme 19.08.2026 12:59

--- relia/sw.js
+++ relia/sw.js
@@ -1,5 +1,5 @@
 /* Relia service worker — her açılışta güncel sürümü getirir */
-const CACHE = 'relia-1.0.0-b2';
+const CACHE = 'relia-1.0.0-b3';
 const SHELL = ['./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo.svg'];
 
 self.addEventListener('install', e => {
