1e37c9eb9383db9cb57adc5059b41b5382e56aa0
oguzcelik93 <oguzcelik93@gmail.com>
2026-08-22 10:19:22 +0200

    relia: güncelleme 22.08.2026 10:19

--- relia/sw.js
+++ relia/sw.js
@@ -1,5 +1,5 @@
 /* Relia service worker — her açılışta güncel sürümü getirir */
-const CACHE = 'relia-1.0.0-b16';
+const CACHE = 'relia-1.0.0-b17';
 /* version.json asla önbelleğe alınmaz */
 const SHELL = ['./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo.svg'];
 
